62b6f15aaa3cf5d4b2b6bb45bca30cd3dc9ff940
VladSaliuk <w62349@student.wsiz.rzeszow.pl>
2020-10-17 16:56:43 +0200

    Initial Commit

---
+++ Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! from the Visual Studio");
+        }
+    }
+}
